c30a334f8fb9b31456b1d669a61d1a7aa801b83d
Rafael Santana <rgs1990419@gmail.com>
2020-07-02 10:16:55 -0300

    Recuperação de senha: Enviando formulário p/ API

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { FiLogIn, FiMail } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
@@ -13,6 +13,8 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import api from '../../services/api';
+
 import {
   Container,
   Content,
@@ -25,6 +27,7 @@ interface ForgotPasswordFormData {
 }
 
 const ForgotPassword: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const formRef = useRef<FormHandles>(null);
 
   const { addToast } = useToast();
@@ -32,12 +35,24 @@ const ForgotPassword: React.FC = () => {
   const handleSubmit = useCallback(
     async (data: ForgotPasswordFormData) => {
       try {
+        setLoading(true);
         formRef.current?.setErrors({});
 
         const schema = Yup.object().shape({
           email: Yup.string()
             .required('E-mail obrigatório')
             .email('Digite um e-mail válido'),
+        });
+
+        await api.post('/forgot/password', {
+          email: data.email,
+        });
+
+        addToast({
+          type: 'success',
+          title: 'Recuperação de senha',
+          description:
+            'Enviamos um e-mail para prosseguir com a recuperação da sua senha',
         });
 
         await schema.validate(data, {
@@ -59,6 +74,8 @@ const ForgotPassword: React.FC = () => {
           description:
             'Ocorreu um erro ao tentar recuperar a senha, tente novamente.',
         });
+      } finally {
+        setLoading(false);
       }
     },
     [addToast],
