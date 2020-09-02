8a08e100de22c4918d0bf7173cc32e1457a9e1b2
Rafael Santana <rgs1990419@gmail.com>
2020-09-02 13:23:53 -0300

    Criando mock da Api

--- src/__tests__/hooks/AuthContext.spec.tsx
+++ src/__tests__/hooks/AuthContext.spec.tsx
@@ -1,9 +1,26 @@
 import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
 import { useAuth, AuthProvider } from '../../hooks/AuthContext';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
+
+const apiResponse = {
+  user: {
+    id: 'user-123',
+    name: 'John Doe',
+    email: 'johndoe@example.com.br',
+  },
+  token: 'token-123',
+};
 
 describe('AuthContext hook', () => {
-  it('should be able to sign in', () => {
-    const { result } = renderHook(() => useAuth(), {
+  it('should be able to sign in', async () => {
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
@@ -11,6 +28,18 @@ describe('AuthContext hook', () => {
       email: 'johndoe@example.com.br',
       password: '123456',
     });
+
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:token',
+      apiResponse.token,
+    );
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:user',
+      JSON.stringify(apiResponse.user),
+    );
 
     expect(result.current.user.email).toEqual('johndoe@example.com.br');
   });
